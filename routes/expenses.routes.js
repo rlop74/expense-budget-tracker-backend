bac9ca97ec5256c6b52a50d35d9efec26edfa658
rlop74 <119091421+rlop74@users.noreply.github.com>
2025-12-07 23:48:40 -0500

    added post method for expenses route

--- routes/expenses.routes.js
+++ routes/expenses.routes.js
@@ -7,11 +7,28 @@ router.get("/", async (req, res) => {
     try {
         const { data, error } = await supabase.from("expenses").select("*");
         if (error) {
-            res.status(400).send(error);
+            return res.status(400).send(error);
         }
-        res.status(200).send(data)
+        res.status(200).send(data);
     } catch (err) {
         return res.status(400).send(err);
+    }
+});
+
+router.post("/add-expense", async (req, res) => {
+    try {
+        const { data, error } = await supabase
+            .from("expenses")
+            .insert(req.body)
+            .select();
+
+        if (error) {
+            return res.status(400).send(error);
+        }
+        console.log(data[0]);
+        res.status(200).json(data[0]);
+    } catch (err) {
+        res.status(400).send(err);
     }
 });
 
