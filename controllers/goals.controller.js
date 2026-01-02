e4083a3e8036865b763c51f53682810ad5ecfcfd
rlop74 <119091421+rlop74@users.noreply.github.com>
2026-01-02 17:00:26 -0500

    built new goal (adding) feature

--- controllers/goals.controller.js
+++ controllers/goals.controller.js
@@ -18,4 +18,20 @@ export class GoalsController {
             return res.status(400).send(err);
         }
     }
+
+    async addGoal(req, res) {
+        try {
+            const { data, error } = await supabase
+                .from("goals")
+                .insert(req.body)
+                .select()
+                .single(); // returns only the object instead of putting it in an array
+            if (error) {
+                return res.status(400).send(error);
+            }
+            res.status(200).send(data);
+        } catch (err) {
+            return res.status(400).send(err);
+        }
+    }
 }
--- routes/goals.routes.js
+++ routes/goals.routes.js
@@ -4,6 +4,9 @@ import { GoalsController } from "../controllers/goals.controller.js";
 const router = Router();
 const goalsController = new GoalsController(); // instantiate class
 
+// http://localhost:3000/goals/
+
 router.get("/", goalsController.getAllGoals.bind(goalsController));
+router.post("/add-goal", goalsController.addGoal.bind(goalsController));
 
 export default router;
